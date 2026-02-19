26dbf21c1d3ea5b2f4961e4a7fee403b1fbc5e6d
Kenzie-Crawford <kenzie.scrawford13@gmail.com>
2026-02-19 14:48:00 -0500

    lesson 1 lab 2 html no css yet

--- rendering-arrays-react/src/Components/Learner.jsx
+++ rendering-arrays-react/src/Components/Learner.jsx
@@ -1,8 +1,9 @@
 import Score from './Score';
+import "./Learner.css";
 
 function Learner(props) {
     return (
-        <div>
+        <div className="learner-card">
             <h2>{props.learner.name}</h2>
             <p>{props.learner.bio}</p>
             {props.learner.scores.map((score, index)=>(
