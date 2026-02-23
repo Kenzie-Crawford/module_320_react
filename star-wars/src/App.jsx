c9724b32dcc9b4a9e0142fda566cbc00fc9089af
Kenzie-Crawford <kenzie.scrawford13@gmail.com>
2026-02-23 16:45:10 -0500

    star wars api lab without styling

---
+++ star-wars/src/App.jsx
@@ -0,0 +1,29 @@
+import { getAllStarships } from './services/sw-api'
+import {useState, useEffect} from 'react'
+import StarshipCard from './components/StarshipCard'
+import './App.css'
+
+export default function App() {
+  const [starships, setStarships] = useState([])
+
+  useEffect(function() {
+    async function fetchData() {
+      const ships = await getAllStarships();
+      setStarships(ships);
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h1>Star Wars Starships</h1>
+      {starships.map((ship,index) => (
+        <StarshipCard key={index} starship={ship} />
+      ))}
+    </div>
+      
+    
+  );
+}
+
+
