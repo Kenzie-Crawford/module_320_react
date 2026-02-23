5b40ff24ea6d7622e2c95a6536126e5aa4767612
Kenzie-Crawford <kenzie.scrawford13@gmail.com>
2026-02-23 16:17:31 -0500

    guided lab for lesson 8

---
+++ cryptocurrencies/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import { Route, Routes} from 'react-router-dom';
+import Currencies from './pages/Currencies';
+import Price from './pages/Price';
+import Main from './pages/Main';
+import Nav from './components/Nav';
+
+import './App.css'
+
+export default function App() {
+ 
+
+  return (
+    <div className="App">
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/currencies" element={<Currencies />} />
+        <Route path="/price/:symbol" element={<Price />} />
+      </Routes>
+    </div>
+  )
+}
+
+
